f933c4be0a016fabb851b9122e332597fefdb364
Danreiss <danreis.ctt@gmail.com>
2022-07-29 11:18:11 -0300

    finalizado-sujeitoamelhorias

---
+++ script.js
@@ -0,0 +1,11 @@
+
+window.addEventListener('keydown', playSound);
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.listkey[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    
+    audio.currentTime = 0;
+    audio.play();
+}
